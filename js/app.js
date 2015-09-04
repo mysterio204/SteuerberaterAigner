c844e9b9ad81627a0465dd278d3f2c94ec8ce1fc
mysterio204 <thiemo.kersting@googlemail.com>
2015-09-04 15:39:51 +0200

    stuff

--- js/app.js
+++ js/app.js
@@ -1,1 +1,5 @@
 $(".button-collapse").sideNav();
+ $(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+  });
