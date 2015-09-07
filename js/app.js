9d12e4bbd3a4885d69bdda595ff9fb8a7d103831
mysterio204 <thiemo.kersting@googlemail.com>
2015-09-07 11:46:41 +0200

    alles voll toll jez

--- js/app.js
+++ js/app.js
@@ -2,4 +2,5 @@ $(".button-collapse").sideNav();
  $(document).ready(function(){
     // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
     $('.modal-trigger').leanModal();
+      $('.slider').slider({full_width: true});
   });
