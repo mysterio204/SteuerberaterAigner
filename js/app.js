44c2393525c865d7c29332fd57fdb1258bcfa686
John Fizz <aigner.jo@gmx.de>
2015-09-10 18:41:00 +0200

    header slider

--- js/app.js
+++ js/app.js
@@ -4,3 +4,25 @@ $(".button-collapse").sideNav();
     $('.modal-trigger').leanModal();
       $('.slider').slider({full_width: true});
   });
+
+
+
+
+window.addEventListener('scroll', function(e){
+        var distanceY = window.pageYOffset || document.documentElement.scrollTop,
+            shrinkOn = 200,
+            header = document.querySelector("nav"),
+            logo = document.querySelector(".head_logo");
+        if (distanceY > shrinkOn) {
+            classie.add(header,"smaller");
+            classie.add(logo,"small");
+        } else {
+            if (classie.has(header,"smaller")) {
+                classie.remove(header,"smaller");
+                
+               classie.remove(logo, "small");
+               
+
+            }
+        }
+    });
