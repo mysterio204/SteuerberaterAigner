19a3cd1929a6e777cb5722b730214dddac6b30d3
mysterio204 <thiemo.kersting@googlemail.com>
2015-09-11 18:22:03 +0200

    header smooth as fuck

--- js/app.js
+++ js/app.js
@@ -13,17 +13,20 @@ window.addEventListener('scroll', function(e){
             shrinkOn = 200,
             header = document.querySelector("header"),
             nav = document.querySelector("nav"),
-            logo = document.querySelector(".head_logo");
+            logo = document.querySelector(".head_logo"),
+    logo2 = document.querySelector(".title_text");
         if (distanceY > shrinkOn) {
             classie.add(header,"smaller");
             classie.add(nav, "smaller");
             classie.add(logo,"small");
+            classie.add(logo2,"small");
         } else {
             if (classie.has(header,"smaller")) {
                 classie.remove(header,"smaller");
                 setTimeout(function(){
                     classie.remove(nav,"smaller");
                classie.remove(logo, "small");
+                    classie.remove(logo2, "small");
                 }, 1);
             }
         }
