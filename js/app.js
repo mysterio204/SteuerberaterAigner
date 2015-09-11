e69c090a10185f7bda4dfcbbe980ce0027e95676
John Fizz <aigner.jo@gmx.de>
2015-09-11 11:19:32 +0200

    header neu

--- js/app.js
+++ js/app.js
@@ -11,17 +11,20 @@ $(".button-collapse").sideNav();
 window.addEventListener('scroll', function(e){
         var distanceY = window.pageYOffset || document.documentElement.scrollTop,
             shrinkOn = 200,
-            header = document.querySelector("nav"),
+            header = document.querySelector("header"),
+            nav = document.querySelector("nav"),
             logo = document.querySelector(".head_logo");
         if (distanceY > shrinkOn) {
             classie.add(header,"smaller");
+            classie.add(nav, "smaller");
             classie.add(logo,"small");
         } else {
             if (classie.has(header,"smaller")) {
                 classie.remove(header,"smaller");
+                setTimeout(function(){
+                    classie.remove(nav,"smaller");
                classie.remove(logo, "small");
-               
-
+                }, 1);
             }
         }
     });
